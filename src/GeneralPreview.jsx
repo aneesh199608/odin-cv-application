24b965ef372b8dc620095d2bc19ae353b2dd2fc8
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-04-30 22:04:30 +0530

    feat: implement GeneralPreview basic logic

---
+++ src/GeneralPreview.jsx
@@ -0,0 +1,8 @@
+export default GeneralPreview({data}) {
+    return (
+        <aside className="preview">
+        <h2>{data.fullName}</h2>
+        <p>{data.jobTitle}</p>
+        </aside>
+    )
+}
