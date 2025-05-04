7bff970585bafb6c0891f60af8f1855c428c81cc
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-05-04 07:30:01 +0530

    chore: add other fields to display

--- src/GeneralPreview.jsx
+++ src/GeneralPreview.jsx
@@ -2,8 +2,26 @@ export default function GeneralPreview({ data }) {
     console.log('Rendering Preview');
     return (
         <aside className="preview">
-        <h3>{data.general.fullName}</h3>
-        <p>{data.general.jobTitle}</p>
+            <div className="preview-header">
+                <h2 className="header-fullName">{data.general.fullName}</h2>
+                <h4 className="header-jobTitle">{data.general.jobTitle}</h4>
+                <div className="header-contact">
+                    <p className="header-email">{data.general.email}</p>
+                    <p className="header-phone">{data.general.phone}</p>
+                    <p className="header-location">{data.general.location}</p>
+                </div>
+            </div>
+            <div className="preview-main">
+                <div className="summary">
+                    <div className="summary-title">
+                        {/* <svg></svg> */}
+                        <h5 className="summary-text">Summary</h5>
+                    </div>
+                    <p className="summary-description">{data.general.summary}</p>
+                </div>
+            </div>
+
+        
         </aside>
     );
 }
